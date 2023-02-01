2e7761ceaa165aceb0a283bcf3357425d3a466b8
Steffengra <steffengrac@gmail.com>
2023-02-01 21:12:53 +0100

    fixed erroneous loop

--- src/data/channel/los_channel_error_model_multiplicative_on_cos.py
+++ src/data/channel/los_channel_error_model_multiplicative_on_cos.py
@@ -22,18 +22,19 @@ def los_channel_error_model_multiplicative_on_cos(
     # calculate indices for steering vectors
     steering_idx = arange(0, satellite.antenna_nr) - (satellite.antenna_nr - 1) / 2
 
+    # TODO: entschleifen für performance
     for user in users:
-        for antenna_idx in range(satellite.antenna_nr):
-            steering_error = exp(
-                steering_idx * (
-                    1j * 2 * pi / satellite.wavelength
-                    * satellite.antenna_distance
-                    * satellite.rng.uniform(low=error_model_config.uniform_error_interval['low'],
-                                            high=error_model_config.uniform_error_interval['high'])
-                )
-
+        steering_error = exp(
+            steering_idx * (
+                1j * 2 * pi / satellite.wavelength
+                * satellite.antenna_distance
+                * satellite.rng.uniform(low=error_model_config.uniform_error_interval['low'],
+                                        high=error_model_config.uniform_error_interval['high'],
+                                        size=satellite.antenna_nr)
             )
 
-            erroneous_channel_state_to_users[user.idx] = satellite.channel_state_to_users[user.idx] * steering_error
+        )
+
+        erroneous_channel_state_to_users[user.idx] = satellite.channel_state_to_users[user.idx] * steering_error
 
     return erroneous_channel_state_to_users
