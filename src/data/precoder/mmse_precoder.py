13d4d9991180cf460745ef94885c57a041c72aed
Steffengra <steffengrac@gmail.com>
2023-02-01 22:01:53 +0100

    fixed error using transpose instead of conj transpose

--- src/data/precoder/mmse_precoder.py
+++ src/data/precoder/mmse_precoder.py
@@ -20,8 +20,8 @@ def mmse_precoder(
 ) -> ndarray:
 
     # TODO: What is lambda? small const for numerical stability?
-    inversion_constant_lambda = finfo('float32').tiny
-    # inversion_constant_lambda = 0
+    # inversion_constant_lambda = finfo('float32').tiny
+    inversion_constant_lambda = 0
 
     user_nr = channel_matrix.shape[0]
     sat_tot_ant_nr = channel_matrix.shape[1]
@@ -30,14 +30,15 @@ def mmse_precoder(
     precoding_matrix = (
         matmul(
             inv(
-                matmul(channel_matrix.T, channel_matrix)
+                matmul(channel_matrix.conj().T, channel_matrix)
                 + (noise_power_watt * (user_nr / power_constraint_watt) + inversion_constant_lambda) * eye(sat_tot_ant_nr)
+                # + (noise_power_watt * (power_constraint_watt / user_nr) + inversion_constant_lambda) * eye(sat_tot_ant_nr)
             ),
-            channel_matrix.T
+            channel_matrix.conj().T
         )
     )
 
-    norm_factor = sqrt(power_constraint_watt / trace(matmul(precoding_matrix.T, precoding_matrix)))
+    norm_factor = sqrt(power_constraint_watt / trace(matmul(precoding_matrix.conj().T, precoding_matrix)))
 
     # TODO: Sind wir ok damit dass der boi komplex ist?
     w_mmse = norm_factor * precoding_matrix
