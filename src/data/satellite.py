816696865a5285b60feb2459fcaeea68ab19f947
Steffengra <steffengrac@gmail.com>
2023-02-01 11:28:52 +0100

    satellite.py holds the satellite class

---
+++ src/data/satellite.py
@@ -0,0 +1,15 @@
+
+from src.utils.spherical_to_cartesian_coordinates import (
+    spherical_to_cartesian_coordinates,
+)
+
+
+class Satellite:
+    def __init__(
+            self,
+            idx,
+            spherical_coordinates,
+    ) -> None:
+        self.idx = idx
+        self.spherical_coordinates = spherical_coordinates
+        self.cartesian_coordinates = spherical_to_cartesian_coordinates(spherical_coordinates)
