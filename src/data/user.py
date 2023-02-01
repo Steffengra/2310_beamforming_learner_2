def26c052dd51f5df66cd8c61f9d7cb81d1ba8f5
Steffengra <steffengrac@gmail.com>
2023-02-01 11:49:25 +0100

    initial users.py holds user.py objects

---
+++ src/data/user.py
@@ -0,0 +1,17 @@
+
+from src.utils.spherical_to_cartesian_coordinates import (
+    spherical_to_cartesian_coordinates,
+)
+
+
+class User:
+
+    def __init__(
+            self,
+            idx,
+            spherical_coordinates,
+    ) -> None:
+
+        self.idx: int = idx
+        self.spherical_coordinates = spherical_coordinates
+        self.cartesian_coordinates = spherical_to_cartesian_coordinates(spherical_coordinates)
---
+++ src/data/users.py
@@ -0,0 +1,69 @@
+
+from numpy import (
+    arange,
+    zeros,
+    ones,
+    sqrt,
+    arccos,
+    pi,
+)
+
+from src.config.config import (
+    Config,
+)
+from src.data.user import (
+    User,
+)
+
+
+class Users:
+    """
+    Satellites holds all satellite objects
+    TODO: And helper functions?
+    """
+
+    def __init__(
+            self,
+            config: Config,
+    ) -> None:
+
+        self.rng = config.rng
+        self.logger = config.logger.getChild(__name__)
+
+        # calculate average user positions
+        user_pos_average = (arange(0, config.user_nr) - (config.user_nr - 1) / 2) * config.user_dist_average
+
+        # add random value on user distances
+        user_dist = user_pos_average + sqrt(config.user_dist_variance) * self.rng.normal(config.user_nr)
+
+        # calculate user_aods_diff_earth_rad
+        user_aods_diff_earth_rad = zeros(config.user_nr)
+
+        for user_idx in range(config.user_nr):
+
+            if user_dist[user_idx] < 0:
+                user_aods_diff_earth_rad[user_idx] = -1 * arccos(1 - 0.5 * (user_dist[user_idx] / config.radius_earth)**2)
+            elif user_dist[user_idx] >= 0:
+                user_aods_diff_earth_rad[user_idx] = arccos(1 - 0.5 * (user_dist[user_idx] / config.radius_earth)**2)
+
+        user_center_aod_earth_rad = config.user_center_aod_earth_deg * pi / 180
+
+        # TODO: if any(sat_pos_average == 0) == 1, vllt Fallunterscheidung für gerade und ungerade
+
+        # calculate user_aods_earth_rad
+        user_aods_earth_rad = user_center_aod_earth_rad + user_aods_diff_earth_rad
+
+        # create user objects
+        user_radii = config.radius_earth * ones(config.user_nr)
+        user_inclinations = pi / 2 * ones(config.user_nr)
+
+        self.users: list = []
+        for user_idx in range(config.user_nr):
+            self.users.append(
+                User(
+                    idx=user_idx,
+                    spherical_coordinates=(user_radii[user_idx],
+                                           user_inclinations[user_idx],
+                                           user_aods_earth_rad[user_idx])
+                )
+            )
