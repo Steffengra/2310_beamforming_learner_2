28033a6c0a4be3c7cf9d5de0316a7171cf98106c
Steffengra <steffengrac@gmail.com>
2023-02-01 12:15:40 +0100

    formatting

--- src/data/satellite.py
+++ src/data/satellite.py
@@ -5,11 +5,13 @@ from src.utils.spherical_to_cartesian_coordinates import (
 
 
 class Satellite:
+
     def __init__(
             self,
             idx,
             spherical_coordinates,
     ) -> None:
-        self.idx = idx
+
+        self.idx: int = idx
         self.spherical_coordinates = spherical_coordinates
         self.cartesian_coordinates = spherical_to_cartesian_coordinates(spherical_coordinates)
