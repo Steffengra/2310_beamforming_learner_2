eec4a14edc45d9e31d139017ebb7d88a6d0afdb6
Steffengra <steffengrac@gmail.com>
2023-02-01 22:06:34 +0100

    removed loop from multiplicative error model for performance

--- src/data/channel/los_channel_error_model_multiplicative_on_cos.py
+++ src/data/channel/los_channel_error_model_multiplicative_on_cos.py
@@ -2,6 +2,7 @@
 from numpy import (
     ndarray,
     arange,
+    tile,
     zeros,
     exp,
     pi,
@@ -17,24 +18,34 @@ def los_channel_error_model_multiplicative_on_cos(
     TODO: erklären
     """
 
-    erroneous_channel_state_to_users: ndarray = zeros((len(users), satellite.antenna_nr), dtype='complex')
-
     # calculate indices for steering vectors
     steering_idx = arange(0, satellite.antenna_nr) - (satellite.antenna_nr - 1) / 2
 
     # TODO: entschleifen für performance
-    for user in users:
-        steering_error = exp(
-            steering_idx * (
+    # erroneous_channel_state_to_users: ndarray = zeros((len(users), satellite.antenna_nr), dtype='complex')
+    # for user in users:
+    #     steering_error = exp(
+    #         steering_idx * (
+    #             1j * 2 * pi / satellite.wavelength
+    #             * satellite.antenna_distance
+    #             * satellite.rng.uniform(low=error_model_config.uniform_error_interval['low'],
+    #                                     high=error_model_config.uniform_error_interval['high'],
+    #                                     size=satellite.antenna_nr)
+    #         )
+    #     )
+    #
+    #     erroneous_channel_state_to_users[user.idx] = satellite.channel_state_to_users[user.idx] * steering_error
+
+    steering_error = exp(
+        tile(steering_idx, (len(users), 1)) * (
                 1j * 2 * pi / satellite.wavelength
                 * satellite.antenna_distance
                 * satellite.rng.uniform(low=error_model_config.uniform_error_interval['low'],
                                         high=error_model_config.uniform_error_interval['high'],
-                                        size=satellite.antenna_nr)
-            )
+                                        size=(len(users), satellite.antenna_nr))
 
         )
-
-        erroneous_channel_state_to_users[user.idx] = satellite.channel_state_to_users[user.idx] * steering_error
+    )
+    erroneous_channel_state_to_users = satellite.channel_state_to_users * steering_error
 
     return erroneous_channel_state_to_users
