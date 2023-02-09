3d9c8e0bab68e7a8479b4e12a2e4dbc694514ea3
Steffengra <steffengrac@gmail.com>
2023-02-09 11:24:35 +0100

    added get_state_aods

--- src/models/get_state.py
+++ src/models/get_state.py
@@ -16,3 +16,12 @@ def get_state_erroneous_channel_state_information(
     state = state.flatten()
 
     return state
+
+
+def get_state_aods(
+        satellites: Satellites,
+) -> ndarray:
+
+    state = satellites.get_aods_to_users()
+
+    return state.flatten()
