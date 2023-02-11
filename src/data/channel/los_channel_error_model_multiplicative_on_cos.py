8aa9c1bf649bb90dc6a827e66de3438c01335aa6
Steffengra <steffengrac@gmail.com>
2023-02-11 23:05:40 +0100

    minor performance on error model

--- src/data/channel/los_channel_error_model_multiplicative_on_cos.py
+++ src/data/channel/los_channel_error_model_multiplicative_on_cos.py
@@ -21,31 +21,15 @@ def los_channel_error_model_multiplicative_on_cos(
     # calculate indices for steering vectors
     steering_idx = arange(0, satellite.antenna_nr) - (satellite.antenna_nr - 1) / 2
 
-    # OLD IMPLEMENTATION - LESS PERFORMANCE
-    erroneous_channel_state_to_users: ndarray = zeros((len(users), satellite.antenna_nr), dtype='complex')
-    for user in users:
-        steering_error = exp(
-            steering_idx * (
-                1j * 2 * pi / satellite.wavelength
-                * satellite.antenna_distance
-                * satellite.rng.uniform(low=error_model_config.uniform_error_interval['low'],
-                                        high=error_model_config.uniform_error_interval['high'],
-                                        size=1)
-            )
+    steering_error = exp(
+        steering_idx * (
+            1j * 2 * pi / satellite.wavelength
+            * satellite.antenna_distance
+            * satellite.rng.uniform(low=error_model_config.uniform_error_interval['low'],
+                                    high=error_model_config.uniform_error_interval['high'],
+                                    size=(len(users), 1))
         )
-
-        erroneous_channel_state_to_users[user.idx] = satellite.channel_state_to_users[user.idx] * steering_error
-
-    # steering_error = exp(
-    #     steering_idx * (
-    #             1j * 2 * pi / satellite.wavelength
-    #             * satellite.antenna_distance
-    #             * satellite.rng.uniform(low=error_model_config.uniform_error_interval['low'],
-    #                                     high=error_model_config.uniform_error_interval['high'],
-    #                                     size=len(users))
-    #
-    #     )
-    # )
-    # erroneous_channel_state_to_users = satellite.channel_state_to_users * steering_error
+    )
+    erroneous_channel_state_to_users = satellite.channel_state_to_users * steering_error
 
     return erroneous_channel_state_to_users
