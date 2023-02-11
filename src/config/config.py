c97846ba01a68eb31fba78ec915ac2d0b0a23687
Steffengra <steffengrac@gmail.com>
2023-02-11 23:04:59 +0100

    collected mmse args

--- src/config/config.py
+++ src/config/config.py
@@ -22,6 +22,9 @@ from src.config.config_error_model import (
 from src.config.config_td3_learner import (
     ConfigTD3Learner,
 )
+from src.config.config_sac_learner import (
+    ConfigSACLearner,
+)
 from src.data.channel.los_channel_model import (
     los_channel_model,
 )
@@ -32,9 +35,10 @@ from src.utils.get_wavelength import (
 
 # TODO: At the moment, the policy network output is softmax. thats not necessary, but if
 #  we change it, we also need to change the scale of the exploration noise somehow
-# TODO: Implement copy config to saved model
 # TODO: improve calc sum rate performance
 # TODO: Fix add random distribution
+
+# l2 penalty auf gewichte?
 
 
 class Config:
@@ -49,7 +53,7 @@ class Config:
         self._pre_init()
 
         # General
-        self.profile: bool = False  # performance profiling
+        self.profile: bool = True  # performance profiling
         self.show_plots: bool = True
 
         self.verbosity: int = 1  # 0 = no prints, 1 = prints
@@ -71,26 +75,26 @@ class Config:
         self.radius_orbit: float = self.altitude_orbit + self.radius_earth  # Orbit radius with Earth center r0, earth centered
 
         # User
-        self.user_nr: int = 4  # Number of users
+        self.user_nr: int = 3  # Number of users
         self.user_gain_dBi: float = 0  # User gain in dBi
-        self.user_dist_average: float = 10_000  # Average user distance in m
-        self.user_dist_variance: float = 0  # Variance of average user distance (normal distribution around the average user distance)
+        self.user_dist_average: float = 1000  # Average user distance in m
+        self.user_dist_variance: float = 0**2  # Variance of average user distance (normal distribution around the average user distance)
         self.user_center_aod_earth_deg: float = 90  # Average center of users
 
         self.user_gain_linear: float = 10**(self.user_gain_dBi / 10)  # User gain linear
 
         # Satellite
-        self.sat_nr: int = 3  # Number of satellites
-        self.sat_tot_ant_nr: int = 12  # Total number of  Tx antennas, should be a number larger than sat nr
+        self.sat_nr: int = 2  # Number of satellites
+        self.sat_tot_ant_nr: int = 6  # Total number of  Tx antennas, should be a number larger than sat nr
         self.sat_gain_dBi: float = 20  # Total sat TODO: Wert nochmal checken
-        self.sat_dist_average: float = 100_000  # Average satellite distance in meter
+        self.sat_dist_average: float = 10_000  # Average satellite distance in meter
         self.sat_dist_variance: float = 0  # Variance of Average satellite distance (normal distribution)
         self.sat_center_aod_earth_deg: float = 90  # Average center of satellites
 
         self.sat_gain_linear: float = 10**(self.sat_gain_dBi / 10)  # Gain per satellite linear
         self.sat_ant_nr: int = int(self.sat_tot_ant_nr / self.sat_nr)  # Number of Tx antennas per satellite
         self.sat_ant_gain_linear: float = self.sat_gain_linear / self.sat_tot_ant_nr  # Gain per satellite antenna
-        self.sat_ant_dist: float = self.wavelength / 2  # Distance between antenna elements in meter
+        self.sat_ant_dist: float = 3 * self.wavelength / 2  # Distance between antenna elements in meter
 
         # Channel Model
         self.channel_model = los_channel_model
@@ -142,6 +146,13 @@ class Config:
 
         self.user_args: dict = {
             'gain_linear': self.user_gain_linear,
+        }
+
+        self.mmse_args: dict = {
+            'power_constraint_watt': self.power_constraint_watt,
+            'noise_power_watt': self.noise_power_watt,
+            'sat_nr': self.sat_nr,
+            'sat_ant_nr': self.sat_ant_nr,
         }
 
     def __logging_setup(
