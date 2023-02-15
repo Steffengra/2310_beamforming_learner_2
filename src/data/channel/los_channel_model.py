0ab7d4145b80b48f94517f67f4e4af68dbd5c328
Steffengra <steffengrac@gmail.com>
2023-02-15 22:19:17 +0100

    reverted to old calculation

--- src/data/channel/los_channel_model.py
+++ src/data/channel/los_channel_model.py
@@ -22,19 +22,22 @@ def los_channel_model(
     TODO: describe this
     """
 
-    amplitude_dampening = [
-        sqrt(
-            satellite.antenna_gain_linear
-            * user.gain_linear
-            * (satellite.wavelength / (4 * pi * satellite.distance_to_users[user.idx])) ** 2
+    channel_state_information = zeros((len(users), satellite.antenna_nr), dtype='complex')
+    for user in users:
+        power_ratio = (
+                satellite.antenna_gain_linear
+                * user.gain_linear
+                * (satellite.wavelength / (4 * pi * satellite.distance_to_users[user.idx])) ** 2
         )
-        for user in users
-    ]
-    phase_shift = satellite.distance_to_users % satellite.wavelength * 2 * pi / satellite.wavelength
-    channel_state_information = (
-            amplitude_dampening
-            * exp(1j * phase_shift)[:, None]
-            * satellite.steering_vectors_to_users
-    )
+        amplitude_damping = sqrt(power_ratio)
+
+        phase_shift = satellite.distance_to_users[user.idx] % satellite.wavelength * 2 * pi / satellite.wavelength
+
+        channel_state_information[user.idx, :] = (
+            amplitude_damping
+            * exp(1j * phase_shift)
+            * satellite.steering_vectors_to_users[user.idx]
+        )
 
     return channel_state_information
+
