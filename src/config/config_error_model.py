e0731d8da2140a72eddc21d457dec16d6f7b8016
Steffengra <steffengrac@gmail.com>
2023-02-19 11:56:25 +0100

    added dummy error model, added update() function

--- src/config/config_error_model.py
+++ src/config/config_error_model.py
@@ -1,8 +1,10 @@
 
+from src.data.channel.los_channel_error_model_no_error import (
+    los_channel_error_model_no_error,
+)
 from src.data.channel.los_channel_error_model_multiplicative_on_cos import (
     los_channel_error_model_multiplicative_on_cos,
 )
-
 from src.data.channel.los_channel_error_model_in_sat2user_dist import (
     los_channel_error_model_in_sat2user_dist,
 )
@@ -17,21 +19,39 @@ class ConfigErrorModel:
             self,
     ) -> None:
 
-        #self.error_model = los_channel_error_model_multiplicative_on_cos
-        self.error_model = los_channel_error_model_in_sat2user_dist
+        # self.error_model = los_channel_error_model_no_error
+        self.error_model = los_channel_error_model_multiplicative_on_cos
+        # self.error_model = los_channel_error_model_in_sat2user_dist
+
+        self.update()
+
+    def _set_params(
+            self,
+    ) -> None:
+
+        # NO ERROR MODEL
+        #  This is a dummy error model
+        if self.error_model == los_channel_error_model_no_error:
+            self.error_model_name = 'err_no'
 
         # MULTIPLICATIVE ERROR MODEL
         #  In this case, the error is not directly added to the AODs but uniformly
         #  distributed on the cos(aods)
         if self.error_model == los_channel_error_model_multiplicative_on_cos:
+            self.error_model_name: str = 'err_mult_on_steering_cos'
             self.uniform_error_interval: dict = {
-                'low': 0,
-                'high': 0,
+                'low': -0.0,
+                'high': 0.0,
             }
 
         # SAT2USER DISTANCE ERROR MODEL
-        # This error model models unknown phase shifts between satellites TODO
+        #  This error model calculates an erroneous channel state information estimate based on a
+        #  perturbed satellite to user distance estimate.
         if self.error_model == los_channel_error_model_in_sat2user_dist:
-            self.distance_error_std: float = 1/100_000_000  # zB 1/100_000_000, 2/100_000_000..
+            self.error_model_name: str = 'err_sat2userdist'
+            self.distance_error_std: float = 0/100_000_000  # zB 1/100_000_000, 2/100_000_000..
 
         # Normal distributed directly on AODs ??? TODO
+
+    def update(self):
+        self._set_params()
