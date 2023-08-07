9aaf386d2dfed736d192c5b874c50b006dcc4752
S <stgr@posteo.net>
2023-08-07 12:53:32 +0200

    removed obsolete tests

--- src/tests/test_scenario.py
+++ src/tests/test_scenario.py
@@ -31,35 +31,6 @@ class TestSystemState(unittest.TestCase):
         self.satellite_manager.update_erroneous_channel_state_information(error_model_config=self.config.error_model,
                                                                           users=self.user_manager.users)
 
-    def test_state_erroneous_csi_rad_phase_normalization(
-            self,
-    ):
-        csi = get_state_erroneous_channel_state_information(
-            satellite_manager=self.satellite_manager,
-            csi_format='rad_phase',
-            norm_state=True,
-        )
-        self.assertTrue(all(abs(csi) < 5))
-
-    def test_state_erroneous_csi_real_imag_normalization(
-            self,
-    ):
-        csi = get_state_erroneous_channel_state_information(
-            satellite_manager=self.satellite_manager,
-            csi_format='real_imag',
-            norm_state=True,
-        )
-        self.assertTrue(all(abs(csi) < 5))
-
-    def test_state_aods_normalization(
-            self,
-    ):
-        aods = get_state_aods(
-            satellite_manager=self.satellite_manager,
-            norm_state=True
-        )
-        self.assertTrue(all(abs(aods) < 5))
-
 
 if __name__ == '__main__':
     unittest.main()
