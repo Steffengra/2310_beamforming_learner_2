fff097c9c2af9f5a9bd0023be7accb626841d22e
S <stgr@posteo.net>
2023-10-22 14:14:56 +0200

    broke up loop for big performance gain

--- src/data/precoder/calc_autocorrelation.py
+++ src/data/precoder/calc_autocorrelation.py
@@ -33,35 +33,40 @@ def calc_autocorrelation(
                 + errors['additive_error_on_cosine_of_aod'][user_id]
         )
 
+        # calc characteristic function all at once for performance reasons
+        antenna_shift_idxs = np.empty((sat_ant_nr, sat_ant_nr))
+        for antenna_row_id in range(sat_ant_nr):
+            for antenna_col_id in range(sat_ant_nr):
+
+                antenna_shift_idxs[antenna_row_id, antenna_col_id] = (
+                    wavenumber * sat_antenna_spacing
+                    * (antenna_col_id - antenna_row_id)
+                )
+
+        if error_distribution == 'uniform':
+            characteristic_functions = np.sinc(
+                antenna_shift_idxs
+                * error_model_config.error_rng_parametrizations['additive_error_on_cosine_of_aod']['args']['high']
+                / np.pi
+            )
+
+        elif error_distribution == 'gaussian':
+            characteristic_functions = np.exp(
+                -antenna_shift_idxs**2
+                * error_model_config.error_rng_parametrizations['additive_error_on_cosine_of_aod']['args']['scale']**2
+                / 2
+            )
+
+        else:
+            raise ValueError('Unknown error distribution on cosine of AODs')
+
+        # calc autocorrelation matrix entries
         for antenna_row_id in range(sat_ant_nr):
 
             for antenna_col_id in range(sat_ant_nr):
-
-                antenna_shift_idx = (
-                        wavenumber * sat_antenna_spacing
-                        * (antenna_col_id - antenna_row_id)
-                )
-
-                if error_distribution == 'uniform':
-                    characteristic_function = np.sinc(
-                        antenna_shift_idx
-                        * error_model_config.error_rng_parametrizations['additive_error_on_cosine_of_aod']['args']['high']
-                        / np.pi
-                    )
-
-                elif error_distribution == 'gaussian':
-                    characteristic_function = np.exp(
-                        -antenna_shift_idx**2
-                        * error_model_config.error_rng_parametrizations['additive_error_on_cosine_of_aod']['args']['scale']**2
-                        / 2
-                    )
-
-                else:
-                    raise ValueError('Unknown error distribution on cosine of AODs')
-
                 autocorrelation_matrix[user_id, antenna_row_id, antenna_col_id] = (
                         np.exp(1j * wavenumber * sat_antenna_spacing * (antenna_row_id - antenna_col_id) * phi_dach)
-                        * characteristic_function
+                        * characteristic_functions[antenna_row_id, antenna_col_id]
                 )
 
     return autocorrelation_matrix
